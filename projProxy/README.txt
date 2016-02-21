77550ae76fbba950a84a2ad0d388213dec49dc41
senghuot <senghuotlim@gmail.com>
2016-02-21 11:31:51 -0800

    cleaning to turn in

---
+++ projProxy/README.txt
@@ -0,0 +1,3 @@
+Chieh Chang 1234767 coralbue@cs.washington.edu
+Senghout Lim 1211352 senghuot@cs.washington.edu
+Drew Dowling 1365636 dowling3@uw.edu
--- projProxy/proxy.js
+++ projProxy/proxy.js
@@ -1,4 +1,4 @@
-var net		= require('net');
+var net     = require('net');
 var message = require('./message');
 
 // used to read buffer and convert to string
@@ -13,9 +13,11 @@ const PROTOCOL = {
     HTTP1_1: 1,
 }
 
+const HTTP_METHODS = ["GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "CONNECT"];
+
 const HTTP_DEFAULT_PORT = 80;
 const HTTPS_DEFAULT_PORT = 443;
-const DEBUG = true;
+const DEBUG = false;
 
 function d_print(message) {
     if (DEBUG) {
@@ -106,15 +108,9 @@ function getRequestProtocolType(header) {
 // version so we don't have to deal with framing issues :)
 function transformHTTPHeader(header) {
     var headerLines = header.split("\r\n");
-    headerLines[0] = setHTTPVersion(headerLines[0], "1.0");
+    headerLines[0] = headerLines[0].replace("HTTP/1.1", "HTTP/1.0");
     headerLines = setConnectionTagClosed(headerLines);
     return headerLines.join("\r\n").concat("\r\n");
-}
-
-function setHTTPVersion(firstLineOfHeader, versionNum) {
-    firstLineSplit = firstLineOfHeader.split(" ");
-    firstLineSplit[2] = "HTTP/" + versionNum;
-    return firstLineSplit.join(" ");
 }
 
 // Takes an array of Strings, representing the lines representing the
@@ -132,7 +128,8 @@ function setConnectionTagClosed(headerLines) {
     return headerLines;
 }
 
-const SERVER_PORT    = 	parseInt(process.argv[2]);
+const PROXY_PORT = parseInt(process.argv[2]);
+const PROXY_HOST = "0.0.0.0";
 
 // reading input form stdin
 if (process.argv.length != 3) {
@@ -140,7 +137,7 @@ if (process.argv.length != 3) {
     process.exit();
 }
 
-if (isNaN(SERVER_PORT)) {
+if (isNaN(PROXY_PORT)) {
     console.log("Usage: port must be numbers only")
     process.exit();
 }
@@ -158,7 +155,7 @@ var tunnelServers = [];*/
 // we need a way to differentiate our client facing sockets
 net.createServer(function(clientSocket) {
     clientSocket.name = clientSocket.remoteAddress + ":" + clientSocket.remotePort;
-    console.log("name of socket " + clientSocket.name);
+    d_print("name of socket " + clientSocket.name);
     clientSocket.on('data', function(data) {
         d_print("RECEIVING DATA");
         if (clientSocket.name in tunnelConnections) {
@@ -167,27 +164,56 @@ net.createServer(function(clientSocket) {
             tunnelConnections[clientSocket.name].write(data);
         } else {
             d_print("DATA FROM ");
-    		var message = decoder.write(data);
-	        var HTTP_method = getRequestMethod(message);
-            //message = transformHTTPHeader(message);
-            
-            //d_print("replace Connection");
+            var message = decoder.write(data);
+            // d_print("transformed HTTP request:");
+            // d_print(message);
 
+        	// spec output
+            firstLineOfHeader = kthLineOfHeader(message, 0).split(" ");
+            printTime(">>> " + firstLineOfHeader[0] + " " + firstLineOfHeader[1]);
 
-	        if (HTTP_method == "CONNECT") {
-	        	d_print("clientSocket received an HTTP CONNECT");
-	        	// attempt to create server facing TCP connection
-	        	initTunnelServerSocket(message, data, clientSocket);
-	        	return;
-	        } else if (HTTP_method == "GET") {
-	        	d_print("clientSocket received an HTTP GET");
-	        }
-	        // if this is the first time receiving data from this client,
-	        // establish a connection to the server it wants to communicate
-	        // with and store the clientSocket mappings
+            var HTTP_method = getRequestMethod(message);
+            if (HTTP_METHODS.indexOf(HTTP_method) < 0) {
+            	// method name doesn't match any in the protocol, so something is wrong.
+            	// close the client, he'll have to try again later
+            	d_print("method was corrupted, so we're not connecting to server");
+            	clientSocket.end();
+            	return;
+            }
+
+            // get info for server connection
+            var host = getRequestHostname(message);
+		    if (host == undefined) {
+		        d_print("host is undefined, here's the message it came from");
+		        d_print(message);
+		        // host is undefined, so something is wrong.
+		        // close the client, he'll have to try again later
+		        d_print("Client hostname was corrupted, so we're not connecting to server");
+		        clientSocket.end();
+		        return;
+		    }
+		    var dstHost = host.hostname;
+		    var dstPort = host.port;
+
+		    var srcHost = PROXY_HOST;
+		    var srcPort = 0; // bind to any port
+
+		    connectObj = {port: dstPort, host: dstHost, };
+
+            if (HTTP_method == "CONNECT") {
+                d_print("clientSocket received an HTTP CONNECT");
+                // attempt to create server facing TCP connection
+                initTunnelServerSocket(connectObj, data, clientSocket);
+                return;
+            } else if (HTTP_method == "GET") {
+                d_print("clientSocket received an HTTP GET");
+            }
+            // if this is the first time receiving data from this client,
+            // establish a connection to the server it wants to communicate
+            // with and store the clientSocket mappings
             //clientSocket.write(new Buffer("data", 'utf-8'));
-	        initNormalServerSocket(message, data, clientSocket);
-    	//}
+            initNormalServerSocket(connectObj, transformHTTPHeader(message), clientSocket);
+        //}
         }
 
 
@@ -195,57 +221,32 @@ net.createServer(function(clientSocket) {
 
     d_print('remote port ' + clientSocket.remotePort);
 
-	// debug for when client disconnects
-	// Emitted when the other end of the socket sends a FIN packet.
-	clientSocket.on('end', function() {
-		d_print(clientSocket.name + " sent a FIN packet.");
-	});
+    // debug for when client disconnects
+    // Emitted when the other end of the socket sends a FIN packet.
+    clientSocket.on('end', function() {
+        d_print(clientSocket.name + " sent a FIN packet.");
+    });
 
-	// Emitted once the socket is fully closed
-	clientSocket.on('close', function(had_error) {
-		// clients.splice(clients.indexOf(clientSocket), 1);
-		d_print(clientSocket.name + " fully closed its connection w/ proxy");
+    // Emitted once the socket is fully closed
+    clientSocket.on('close', function(had_error) {
+        // clients.splice(clients.indexOf(clientSocket), 1);
+        d_print(clientSocket.name + " fully closed its connection w/ proxy");
         if (clientSocket.name in tunnelConnections) {
             tunnelConnections[clientSocket.name].end();
             delete tunnelConnections[clientSocket.name]
         }
-		/*if (tunnelClients[clientSocket] in tunnelServers) {
-			delete tunnelServers[tunnelClients[clientSocket]];
-			tunnelClients[clientSocket].end();
-		}
-		if (clientSocket in tunnelClients) {
-			delete tunnelClients[clientSocket];
-		}*/
-	});
-
-	clientSocket.on('error', function(errorObj) {
-		d_print("clientSocket encountered an error: " + errorObj);
-	});
-
-}).listen(SERVER_PORT);
-
-function initTunnelServerSocket(message, data, clientSocket) {    
-    var serverSocket = new net.Socket();
-    var host = getRequestHostname(message);
-    if (host == undefined) {
-        d_print("host is undefined, here's the message it came from");
-        d_print(message);
-    }
-    var dstHost = host.hostname;
-    var dstPort = host.port;
-
-    var srcHost = "0.0.0.0";
-    var srcPort = 0; // bind to any port
-
-    d_print("starting tunnel to: " + dstHost + ":" + dstPort);
-    // can the ip be the problem?
-    serverSocket.connect({port: dstPort, host: dstHost}, function() {
-        d_print("connected to the server");
-        d_print(clientSocket.name);  
-        var buf = new Buffer("HTTP/1.0 200 OK \r\n\r\n");
-        clientSocket.write(buf);
-        tunnelConnections[clientSocket.name] = serverSocket;
     });
+
+    clientSocket.on('error', function(errorObj) {
+        d_print("clientSocket encountered an error: " + errorObj);
+    });
+
+}).listen(PROXY_PORT);
+
+function initTunnelServerSocket(connectObj, data, clientSocket) {
+    var serverSocket = new net.Socket();
+
+    d_print("starting tunnel to: " + connectObj.dstHost + ":" + connectObj.dstPort);
 
     serverSocket.on("data", function(data) {
         if (DEBUG) {
@@ -278,26 +279,21 @@ function initTunnelServerSocket(message, data, clientSocket) {
         d_print("SOCKETED END");
     });
 
+    // can the ip be the problem?
+    serverSocket.connect(connectObj, function() {
+        d_print("connected to the server");
+        d_print(clientSocket.name);
+        var buf = new Buffer("HTTP/1.0 200 OK \r\n\r\n");
+        clientSocket.write(buf);
+        tunnelConnections[clientSocket.name] = serverSocket;
+    });
 
     return serverSocket;
 }
 
-
-// helper function to print message with time
-var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-];
-
-function printTime(message) {
-    var now = new Date();
-    var timeOutput = now.getDate() + " " + monthNames[now.getMonth()] + " ";
-    timeOutput += now.toLocaleTimeString() + " - ";
-    d_print(timeOutput + message);
-}
-
-function initNormalServerSocket(message, data, clientSocket) {
-	// create a clientSocket to talk to the server, store mappings
-	d_print("init normal server socket");
+function initNormalServerSocket(connectObj, data, clientSocket) {
+    // create a clientSocket to talk to the server, store mappings
+    d_print("init normal server socket");
     var serverSocket = new net.Socket();
     serverSocket.setTimeout(3000);
 
@@ -313,32 +309,61 @@ function initNormalServerSocket(message, data, clientSocket) {
     serverSocket.on('connect', function () {
         d_print("proxy has connected to server");
         // upon connection, send our data to the server
+        d_print("data proxy is sending to server:");
+        //d_print(decoder.write(data)); // this breaks the build ....
         serverSocket.setTimeout(0); // disables
-        serverSocket.write(new Buffer(message, 'utf-8'));
+        serverSocket.write(new Buffer(data, 'utf-8'));
     });
 
     // if we receive any information back from the server,
     // shovel back any bytes to our client
     serverSocket.on('data', function (serverData) {
         //var buf = new Buffer(transformHTTPHeader("" + data));
+        d_print("normal server data");
+        // d_print(decoder.write(serverData)); // this breaks the build
+
+        var endOfHeader = serverData.indexOf('\r\n\r\n');
+        if (endOfHeader > -1) {
+            // message header of response found
+        
+            // transform header
+            var headerBuf = serverData.slice(0, endOfHeader);
+            var bodyBuf = serverData.slice(endOfHeader+2);
+            d_print("header of response");
+            d_print(decoder.write(headerBuf));
+        
+            d_print("body of response");
+        
+            var transformedHeader = transformHTTPHeader(decoder.write(headerBuf));
+            d_print("transformed response header");
+            d_print(transformedHeader);
+        
+            var transformedHeaderBuf = new Buffer(transformedHeader);
+            var transformedMessage = Buffer.concat([transformedHeaderBuf, bodyBuf]);
+            d_print("transformed message");
+            d_print(decoder.write(transformedMessage));
+            serverData = transformedMessage;
+        }
+
         clientSocket.write(serverData);
     })
 
-    // connect to host:port defined in HTTP request
-    var host = getRequestHostname(message);
-    if (host == undefined) {
-    	d_print("host is undefined, here's the message it came from");
-    	d_print(message);
-    }
-    var dstHost = host.hostname;
-    var dstPort = host.port;
-
-    var srcHost = "0.0.0.0";
-    var srcPort = 0; // bind to any port
 
     // if you use google's ip: 8.8.8.8 you get unreachable destination
-    serverSocket.connect({port: dstPort, host: dstHost, localAddress: srcHost, localPort: srcPort});
+    serverSocket.connect(connectObj);
 }
 
 
-printTime('Proxy listening on ' + SERVER_PORT);
+// helper function to print message with time
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+function printTime(message) {
+    var now = new Date();
+    var timeOutput = now.getDate() + " " + monthNames[now.getMonth()] + " ";
+    timeOutput += now.toLocaleTimeString() + " - ";
+    console.log(timeOutput + message);
+}
+
+printTime('Proxy listening on ' + PROXY_HOST + ':' + PROXY_PORT);
